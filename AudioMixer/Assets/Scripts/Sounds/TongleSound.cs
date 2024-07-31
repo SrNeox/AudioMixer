bd915eb6540d825bb91d20bf05589fb298bdb338
SrNeox <gsabinskij33@gmail.com>
2024-07-31 15:30:53 +0300

    Update

--- AudioMixer/Assets/Scripts/Sounds/TongleSound.cs
+++ AudioMixer/Assets/Scripts/Sounds/TongleSound.cs
@@ -1,16 +1,39 @@
 using TMPro;
 using UnityEngine;
+using UnityEngine.Audio;
 
 public class TongleSound : MonoBehaviour
 {
+    [SerializeField] private AudioMixer _audioMixer;
     [SerializeField] private TextMeshProUGUI _textOnOff;
 
-    private bool _isSoundOn = true;
+    public bool _isSoundOn { get; private set; }
+
+
+    private void Start()
+    {
+        _isSoundOn = true;
+    }
 
     public void ToggleSound()
     {
         _isSoundOn = !_isSoundOn;
-        AudioListener.volume = _isSoundOn ? 1 : 0;
+
+        if (_isSoundOn == false)
+        {
+            int volumeOff = -80;
+
+            _audioMixer.SetFloat("MasterVolume", volumeOff);
+        }
+        else
+        {
+            int volumeDefault = 0;
+
+            _audioMixer.SetFloat("MasterVolume", volumeDefault);
+        }
+
         _textOnOff.text = _isSoundOn ? "Выкл звук" : "Вкл звук";
     }
+
+
 }
--- AudioMixer/Assets/Scripts/Sounds/VolumeСhangerAudioMixer.cs
+++ AudioMixer/Assets/Scripts/Sounds/VolumeСhangerAudioMixer.cs
@@ -3,14 +3,40 @@ using UnityEngine.Audio;
 
 public class VolumeСhangerAudioMixer : MonoBehaviour
 {
-    [SerializeField] private string _nameGroupAudioMixer;
     [SerializeField] private AudioMixer _audioMixer;
+    [SerializeField] private TongleSound _tongleSound;
+
+    private string _groupMasterVolume = "MasterVolume";
+    private string _groupButtonsVolume = "ButtonsVolume";
+    private string _groupMusicVolume = "MusicVolume";
 
     private float _minValue = 0.0001f;
+    private float _decibelConversion = 20f;
 
-    public void ChangeVolume(float volume)
+    public void ChangeMasterVolume(float volume)
     {
-        volume = Mathf.Max(volume, _minValue);
-        _audioMixer.SetFloat(_nameGroupAudioMixer, Mathf.Log10(volume) * 20);
+        if (_tongleSound._isSoundOn != false)
+        {
+            volume = Mathf.Max(volume, _minValue);
+            _audioMixer.SetFloat(_groupMasterVolume, Mathf.Log10(volume) * _decibelConversion);
+        }
+    }
+
+    public void ChangeButtonsVolume(float volume)
+    {
+        if (_tongleSound._isSoundOn != false)
+        {
+            volume = Mathf.Max(volume, _minValue);
+            _audioMixer.SetFloat(_groupButtonsVolume, Mathf.Log10(volume) * _decibelConversion);
+        }
+    }
+
+    public void ChangeMusicVolume(float volume)
+    {
+        if (_tongleSound._isSoundOn != false)
+        {
+            volume = Mathf.Max(volume, _minValue);
+            _audioMixer.SetFloat(_groupMusicVolume, Mathf.Log10(volume) * _decibelConversion);
+        }
     }
 }
